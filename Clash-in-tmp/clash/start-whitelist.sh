1450f5596f85b49bf8311a22a205737bf8f9a133
Geralt <ntgeralt@163.com>
2019-11-15 11:28:51 +0800

    update

--- Clash-in-tmp/clash/start-whitelist.sh
+++ Clash-in-tmp/clash/start-whitelist.sh
@@ -7,6 +7,10 @@ iptables -t nat -D PREROUTING CLASH  >/dev/null 2>&1
 iptables -t nat -D OUTPUT CLASH  >/dev/null 2>&1
 /bin/iptables -t nat -F CLASH >/dev/null 2>&1
 /bin/iptables -t nat -X CLASH >/dev/null 2>&1
+/sbin/ipset destroy chnroute >/dev/null 2>&1
+ 
+ipset -exist create chnroute hash:net hashsize 64
+sed -e "s/^/add chnroute /" /tmp/clash/chnroute.txt | ipset restore
 
 
 iptables -t nat -N CLASH
@@ -14,30 +18,32 @@ iptables -t nat -A CLASH -d 0.0.0.0 -j RETURN
 iptables -t nat -A CLASH -d 127.0.0.1 -j RETURN
 iptables -t nat -A CLASH -d 192.168.0.0/16 -j RETURN
 
-# Anything else should be redirected to clash
+
+iptables -t nat -A CLASH -m set --match-set chnroute dst -j RETURN
+
+# Anything else should be redirected to Dokodemo-door's local port
 
 #iptables -t nat -A CLASH -p tcp --dport 22:500 -j REDIRECT --to-ports 7892
-#iptables -t nat -A CLASH -p tcp --dport 22 -j REDIRECT --to-ports 7892
-#iptables -t nat -A CLASH -p tcp --dport 80 -j REDIRECT --to-ports 7892
-#iptables -t nat -A CLASH -p tcp --dport 443 -j REDIRECT --to-ports 7892
-iptables -t nat -A CLASH -p tcp -j REDIRECT --to-ports 7892
+iptables -t nat -A CLASH -p tcp --dport 22 -j REDIRECT --to-ports 7892
+iptables -t nat -A CLASH -p tcp --dport 80 -j REDIRECT --to-ports 7892
+iptables -t nat -A CLASH -p tcp --dport 443 -j REDIRECT --to-ports 7892
+#iptables -t nat -A CLASH -p tcp -j REDIRECT --to-ports 7892
 
 iptables -t nat -A PREROUTING -p tcp -j CLASH
 
 sleep 1
-
 pid=$(ps | awk '/[c]lash /{print $1}')
 if [ "$pid" == "" ]; then
-echo ""
 echo "clash started"
 else
-echo ""
  echo "clash restarted"
 kill $pid
 fi
-cd /tmp/clash/ 
+#cd /tmp/clash/ 
 #SSL_CERT_FILE=./cacert.pem ./clash --config=/tmp/clash/config.yaml &
 /tmp/clash/clash -d /tmp/clash >/dev/null 2>&1 &
+
+
 
 echo "-----------------------------------------------------------------"
 echo ""
