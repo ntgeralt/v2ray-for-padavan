b4a53398262efe5b3c782e1d4c60a92b8e3fc097
Geralt <ntgeralt@users.noreply.github.com>
2019-12-20 16:46:38 +0800

    Update start.sh
    添加注释

--- other/FOR-onekey-shell-tmp/start.sh
+++ other/FOR-onekey-shell-tmp/start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-#echo "Load modprobe"
+#载入modprobe模块
 modprobe ip_set_hash_net
 modprobe xt_set
 
+#结束v2ray-watchdog进程
 while [ -n "`pidof v2ray-watchdog`" ] ; do
 	kill -9 "`pidof v2ray-watchdog`"
 	sleep 1
@@ -11,18 +12,17 @@ echo "v2ray-watchdog killed."
 
 
 
-#echo "cleanning iptables "
+#清理V2RAY iptable
 iptables -t nat -D PREROUTING V2RAY  >/dev/null 2>&1
 iptables -t nat -D OUTPUT V2RAY  >/dev/null 2>&1
 /bin/iptables -t nat -F V2RAY >/dev/null 2>&1
 /bin/iptables -t nat -X V2RAY >/dev/null 2>&1
 /sbin/ipset destroy chnroute >/dev/null 2>&1
-#echo "ipset chnroute"
+
+#iptable绕过大陆ip和内网
 ipset -exist create chnroute hash:net hashsize 64
 sed -e "s/^/add chnroute /" /tmp/v2ray/chnroute.txt | ipset restore
-echo ""
-echo ""
-echo "[setting iptables]"
+
 iptables -t nat -N V2RAY
 iptables -t nat -A V2RAY -d 0.0.0.0 -j RETURN
 iptables -t nat -A V2RAY -d 127.0.0.1 -j RETURN
@@ -31,8 +31,8 @@ iptables -t nat -A V2RAY -d 192.168.0.0/16 -j RETURN
 
 iptables -t nat -A V2RAY -m set --match-set chnroute dst -j RETURN
 
-# Anything else should be redirected to Dokodemo-door's local port
 
+# 其余22-500端口交给v2ray
 iptables -t nat -A V2RAY -p tcp --dport 22:500 -j REDIRECT --to-ports 1234
 #iptables -t nat -A V2RAY -p tcp --dport 22 -j REDIRECT --to-ports 1234
 #iptables -t nat -A V2RAY -p tcp --dport 80 -j REDIRECT --to-ports 1234
@@ -43,7 +43,7 @@ iptables -t nat -A PREROUTING -p tcp -j V2RAY
 
 
 
-
+#关闭v2ray
 pid=$(ps | awk '/[v]2ray --config/{print $1}')
 
 if [ "$pid" == "" ]; then
@@ -54,31 +54,14 @@ echo ""
 echo "[V2Ray restarted]"
     kill $pid
 fi
-echo "-----------------[V2Ray started]---------------------------"
-echo ""
-echo ""
-echo "#stop v2ray"
-echo "sh /tmp/v2ray/stop.sh"
-echo ""
-echo "#Default config.json use GFW Mode"
-echo ""
-echo "Chnroute file at /tmp/v2ray/example-config"
-echo ""
-echo "#Autoruns:"
-echo "'Scripts' - 'Run After Firewall Rules Restarted'"
-echo "wget -O - d.oo14.com/e7XT | bash"
-echo ""
-echo "USE 5353 DNS FOR GFWLIST:"
-echo "Custom 'dnsmasq.conf'"
-echo "conf-dir=/etc/storage/v2ray/, *.hosts"
-echo ""
-echo "-------------you can close this Window---------------------"
-echo ""
-echo ""
 
+echo "-----------------[V2Ray started]---------------------------"
+
+#启动v2ray
 cd /tmp/v2ray/ 
 
 SSL_CERT_FILE=./cacert.pem ./v2ray --config=/etc/storage/v2ray/config.json >/dev/null 2>&1 &
 
+#启动v2ray-watchdog
 /tmp/v2ray/v2ray-watchdog >/dev/null 2>&1 &
 echo "v2ray-watchdog started."
